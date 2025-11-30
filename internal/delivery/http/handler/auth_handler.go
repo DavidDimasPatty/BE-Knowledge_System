ab3a0c3a3537de24c5746e430d8ef7cab36e8fc6
Nando WItin <108390976+nandowitin0602@users.noreply.github.com>
2025-11-30 14:36:17 +0700

    Revisi tracelog

--- internal/delivery/http/handler/auth_handler.go
+++ internal/delivery/http/handler/auth_handler.go
@@ -20,24 +20,24 @@ func NewAuthHandler(userService usecases.UserService) *AuthHandler {
 func (h *AuthHandler) Login(c *gin.Context) {
 	var namaEndpoint = "Login"
 
-	Tracelog.WriteLog("Mulai proses Login", namaEndpoint)
+	Tracelog.AuthLog("Mulai proses Login", namaEndpoint)
 
 	var req dto.Auth_Login_Request
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		Tracelog.WriteLog("Request tidak valid", namaEndpoint)
+		Tracelog.AuthLog("Request tidak valid", namaEndpoint)
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
 		return
 	}
 
 	user, err := h.userService.Login(req.Username, req.Password)
 	if err != nil {
-		Tracelog.WriteLog("Login gagal: "+err.Error(), namaEndpoint)
+		Tracelog.AuthLog("Login gagal: "+err.Error(), namaEndpoint)
 		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		return
 	}
 
-	Tracelog.WriteLog("Login berhasil", namaEndpoint)
+	Tracelog.AuthLog("Login berhasil", namaEndpoint)
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Success",
---
+++ internal/tracelog/tracelog.go
@@ -0,0 +1,39 @@
+package tracelog
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+func write(module string, message string, endpoint string) {
+	_ = os.MkdirAll("logs", os.ModePerm)
+
+	date := time.Now().Format("2006-01-02")
+	filename := fmt.Sprintf("logs/%s_%s.txt", module, date)
+
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Printf("Gagal membuka file log: %v", err)
+		return
+	}
+	defer file.Close()
+
+	logger := log.New(file, "", 0)
+
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	line := fmt.Sprintf("%s --- %s --- Endpoint: %s", timestamp, message, endpoint)
+
+	logger.Println(line)
+}
+
+// Untuk Auth
+func AuthLog(message, endpoint string) {
+	write("tracelog_auth", message, endpoint)
+}
+
+// Untuk User Management
+func UserManagementLog(message, endpoint string) {
+	write("tracelog_user_management", message, endpoint)
+}
